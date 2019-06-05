96143e79d7ee9b578250a2d59cc2a70f42749ac9
Erik Irgens and Holden Clark <erik@>
2019-06-05 11:03:00 -0700

    add css folder and files, add front end JavaScript for basic calculator user interface, updated HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,35 @@
+//back end javascript
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
 
 var subtract = function(number1, number2) {
   return number1 - number2;
-}
+};
 
 var multiply = function(number1, number2) {
   return number1 * number2;
-}
+};
 
 var divide = function(number1, number2) {
   return number1 / number2;
-}
+};
 
-var number1 = parseInt(prompt('Enter a number:'));
-var number2 = parseInt(prompt('Enter a second number:'));
 
-alert(add(number1, number2));
-alert(subtract(number1, number2));
-alert(multiply(number1, number2));
-alert(divide(number1, number2));
+//front end javascript below this comment
+
+$(function(){
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+
+    // alert(add(number1, number2));
+    // alert(subtract(number1, number2));
+    // alert(multiply(number1, number2));
+    // alert(divide(number1, number2));
+  });
+});
